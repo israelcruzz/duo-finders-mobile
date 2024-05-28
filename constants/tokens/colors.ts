d12aa0d366f56f116e71893fa3c9b949daebdfac
israelcruzz <israelcruz.contato@gmail.com>
2024-05-27 23:47:57 -0300

    feat: initial settings and add tokens, images and layout

---
+++ constants/tokens/colors.ts
@@ -0,0 +1,12 @@
+export const colors = {
+  background: "#121212",
+  darkBackground: "#18181B",
+  primary: "#650C71",
+  secondary: "#B7ABFF",
+  green: "#34D399",
+  darkGray: "#282B30",
+  lightGray: "#2A2634",
+  gray: "#525252",
+  lightDarkGray: "#B8B8B8",
+  sub: "#D4D4D8",
+};
---
+++ constants/tokens/size.ts
@@ -0,0 +1,8 @@
+export const size = {
+  title: 32,
+  heading: 18,
+  normal: 16,
+  med: 14,
+  sub: 12,
+  min: 10,
+};
