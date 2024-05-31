de1bc8b2bd0ce8770f4b4d94c9aa5c2b58c4d254
israelcruzz <israelcruz.contato@gmail.com>
2024-05-31 01:11:50 -0300

    feat: add header in game screen

--- app/game/[id]/index.tsx
+++ app/game/[id]/index.tsx
@@ -1,7 +1,7 @@
 import ButtonBack from "@/components/button-back";
 import { tokens } from "@/constants/tokens";
 import { useRoute } from "@react-navigation/native";
-import { StyleSheet, Text, View } from "react-native";
+import { Image, StyleSheet, Text, View } from "react-native";
 
 interface GameParams {
   key: string;
@@ -16,9 +16,15 @@ export default function Game({}) {
 
   return (
     <View style={styles.container}>
-      <Text>{params.id}</Text>
-
-      <ButtonBack />
+      <View style={styles.areaPhotoGame}>
+        <Image
+          style={styles.photoGame}
+          source={require("@/app/assets/images/league-of-legends-game.png")}
+        />
+        <View style={styles.buttonBackArea}>
+          <ButtonBack />
+        </View>
+      </View>
     </View>
   );
 }
@@ -30,5 +36,18 @@ const styles = StyleSheet.create({
     paddingVertical: 44,
     paddingHorizontal: 24,
     gap: 24,
+  },
+  areaPhotoGame: {
+    width: "100%",
+  },
+  photoGame: {
+    width: "100%",
+    borderRadius: 20,
+    objectFit: "cover",
+  },
+  buttonBackArea: {
+    position: "absolute",
+    top: 0,
+    margin: 12,
   },
 });
